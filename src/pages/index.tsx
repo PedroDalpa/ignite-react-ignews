3446abd54628626eeac7298663d580c83017a4bf
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-05-24 11:17:06 -0300

    create subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
@@ -15,6 +17,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <Image src="/images/avatar.svg" alt="garota programando em uma mesa com cafe e livros" width={600} height={600} />
       </main>
