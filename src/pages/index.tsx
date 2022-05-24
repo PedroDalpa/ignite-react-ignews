d11b65298db07e9ebec4324b57d398af429ba93b
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-05-24 10:54:09 -0300

    edit home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Word</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image src="/images/avatar.svg" alt="garota programando em uma mesa com cafe e livros" width={600} height={600} />
+      </main>
     </>
   )
 }
