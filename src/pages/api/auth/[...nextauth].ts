16c06d10726b6a4af0e6af6990cc01d0e2fcc6d1
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-05-25 10:29:06 -0300

    save user in fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
-
+import { fauna } from '../../../services/fauna'
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -13,6 +13,24 @@ export default NextAuth({
         }
       }
     }),
-    // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch (error) {
+        return false
+      }
+
+    }
+  }
 })
