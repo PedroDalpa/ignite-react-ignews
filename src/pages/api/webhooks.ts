890c687c4dcb2d36ac1c1b9f25856103733ede59
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-05-26 21:18:59 -0300

    make a webhook

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,75 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
+
+async function buffer(readable: Readable) {
+
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
+  }
+  return Buffer.concat(chunks);
+}
+
+const relevantEvent = new Set([
+  'checkout.session.completed'
+]);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function stripeWebHook(request: NextApiRequest, response: NextApiResponse) {
+  if (request.method === "POST") {
+    const buf = await buffer(request);
+    const secret = request.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+
+    } catch (err) {
+      return response.status(400).send(`Webhook error ${err.message}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvent.has(type)) {
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch (error) {
+        // ler sobre sentry, bugsnag
+        response.send('Webhook handler failed');
+      }
+
+
+    }
+
+    return response.send('foi');
+  } else {
+    response.setHeader('Allow', 'POST');
+    response.status(405).end('Method not allowed');
+  }
+
+
+}
