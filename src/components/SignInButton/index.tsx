06ef07d48a028e5307c28f55a450a33c8a6e69bb
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-05-26 21:20:12 -0300

    remove log

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,7 +4,6 @@ import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 export function SignInButton() {
   const { data: session } = useSession();
-  console.log(session);
 
   return session ? (
     <button
