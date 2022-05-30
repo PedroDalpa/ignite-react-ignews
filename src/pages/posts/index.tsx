191f928ae31bc93ca1e1d1994b03e0fcaefbad50
Pedro Dalpa <pedro.dalpa@mercadolivre.com>
2022-05-30 15:23:36 -0300

    get posts of prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,21 @@
 
+import { asText } from '@prismicio/helpers';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
-export default function Post() {
+
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Post({ posts }: PostsProps) {
 
   return (
     <>
@@ -10,24 +24,49 @@ export default function Post() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de março de 2022</time>
-            <strong>LERO LEROO LELRKER</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur voluptatibus impedit, in alias nam culpa quasi architecto ducimus ex, deserunt ipsum. Debitis ad quod, perspiciatis tempore error blanditiis soluta.</p>
-          </a>
-          <a href="">
-            <time>12 de março de 2022</time>
-            <strong>LERO LEROO LELRKER</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur voluptatibus impedit, in alias nam culpa quasi architecto ducimus ex, deserunt ipsum. Debitis ad quod, perspiciatis tempore error blanditiis soluta.</p>
-          </a>
-          <a href="">
-            <time>12 de março de 2022</time>
-            <strong>LERO LEROO LELRKER</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur voluptatibus impedit, in alias nam culpa quasi architecto ducimus ex, deserunt ipsum. Debitis ad quod, perspiciatis tempore error blanditiis soluta.</p>
-          </a>
+          {posts.map(post => (
+            <a href="" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+
+          ))}
+
         </div>
       </main>
     </>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getAllByType('post', {
+    fetch: [
+      'Post.Title', 'Post.Content'
+    ],
+    pageSize: 20,
+  });
+
+  const posts = response.map(post => {
+    return {
+      slug: post.uid,
+      title: asText(post.data.Title),
+      excerpt: post.data.Content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+
+  return {
+    props: {
+      posts
+    }
+  }
 
 }
